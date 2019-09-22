2493706a278ac652ce6fa848c722888e366347ae
Siarhei Dzeraviannik <33173033+dsv-sergey@users.noreply.github.com>
2019-09-23 01:33:58 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let first_arr = first.split("").reverse(),
+    second_arr = second.split("").reverse(),
+    stack = [];
+  for (let i = 0; i < first_arr.length; i++) {
+    for (let j = 0; j < second_arr.length; j++) {
+      let mul = first_arr[i] * second_arr[j];
+      stack[i + j] = stack[i + j] ? stack[i + j] + mul : mul;
+    }
+  }
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10,
+      up = Math.floor(stack[i] / 10);
+    stack[i] = num;
+    if (stack[i + 1]) stack[i + 1] += up;
+    else if (up != 0) stack[i + 1] = up;
+  }
+  return stack.reverse().join("");
+};
